b7e647a4952dde6bfeed7539d514b0a091a2ad93
EthanPrieur <ethan.prieur@ocsbstudent.ca>
2021-01-20 14:57:57 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('btn').addEventListener('click', myFunction)
+function myFunction() {
+let side = document.getElementById('box').value
+side = parseInt(side)
+let answer = side * side
+answer = parseInt(answer)
+  alert(answer)
+}
